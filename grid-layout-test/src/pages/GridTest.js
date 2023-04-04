6b4bd3e4631af55e1dd0d6ccd9e5c716aecd13e7
h-amjad <hamjad@horizech.com>
2023-04-04 17:17:35 +0500

    updated grid movement

--- grid-layout-test/src/pages/GridTest.js
+++ grid-layout-test/src/pages/GridTest.js
@@ -1,15 +1,29 @@
+import { useEffect, useState } from "react";
 import GridLayout from "react-grid-layout";
 import styles from "/node_modules/react-grid-layout/css/styles.css"
 import "/node_modules/react-resizable/css/styles.css"
 
 const GridTest = () => {
-
+    const [newKey, setNewKey] = useState("b");
+    
+    const [isStatic, setStatic] = useState(true);
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
         { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
         { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
         { i: "c", x: 4, y: 0, w: 1, h: 2 }
     ];
+    const changeStatic = (e) => {
+        e.preventDefault();
+        !isStatic ? setStatic(true) : setStatic(false);
+        isStatic ? setNewKey("a") : setNewKey("b");
+        console.log("static", isStatic);
+    }
+    useEffect(() => {
+        console.log("hello")
+        
+        console.log("newKey", newKey);
+    }, [isStatic, newKey])
     return (
         <GridLayout
             className={styles.layout}
@@ -17,21 +31,64 @@ const GridTest = () => {
             cols={12}
             rowHeight={400}
             width={1800}
-            style={{ backgroundColor: 'green' }}
+            // isDraggable={!isStatic}
         >
-            <span key="a" >
+            {/* <GridConponent
+                layout={layout}
+                isStatic={isStatic}
+                setStatic={setStatic}
+                key="n"
+            ></GridConponent> */}
+            <div
+                key={"n"}
+                style={{ backgroundColor: 'green'}}
+                draggable={!isStatic}
+            >
+                <button onClick={(e) => changeStatic(e)} >Pin</button>
                 <GridLayout
                     className="layout"
                     layout={layout}
-                    cols={12}
+                    cols={5}
                     rowHeight={30}
-                    width={1200}
-                    key="b"
+                    width={440}
+                    isDraggable={isStatic}
                 >
-                    <div key="g" style={{ backgroundColor: 'skyblue' }}>g</div>
-                    <div key="d" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} style={{ backgroundColor: 'skyblue' }}>d</div>
+                    <div key="g"  style={{ backgroundColor: 'skyblue' }}>g</div>
+
+                    <div key="d" style={{ backgroundColor: 'skyblue' }}>d</div>
                 </GridLayout>
-                {/* <GridLayout
+            </div>
+            <div key="i" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} style={{ backgroundColor: 'orange' }}>b</div>
+            <div key="j" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} style={{ backgroundColor: 'maroon' }}>c</div>
+        </GridLayout>
+    );
+}
+
+const GridConponent = ({ isStatic, setStatic, layout }) => {
+    const changeStatic = (e) => {
+        e.preventDefault();
+        !isStatic ? setStatic(true) : setStatic(false);
+        console.log("static", isStatic);
+    }
+    return (
+        <div
+            data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }}
+            style={{ backgroundColor: 'green', width: "1200" }}
+        >
+            <button onClick={(e) => changeStatic(e)} >Pin</button>
+            <GridLayout
+                className="layout"
+                layout={layout}
+                cols={12}
+                rowHeight={30}
+                width={1200}
+
+            >
+                <div key="g" style={{ backgroundColor: 'skyblue' }}>g</div>
+
+                <div key="d" style={{ backgroundColor: 'skyblue' }}>d</div>
+            </GridLayout>
+            {/* <GridLayout
                     className="layout"
                     layout={layout}
                     cols={12}
@@ -53,10 +110,7 @@ const GridTest = () => {
                 >
                     <div key="h" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} style={{ backgroundColor: 'red' }}>c</div>
                 </GridLayout> */}
-            </span>
-            <div key="i" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} style={{ backgroundColor: 'orange' }}>b</div>
-            <div key="j" data-grid={{ x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }} style={{ backgroundColor: 'maroon' }}>c</div>
-        </GridLayout>
-    );
+        </div>
+    )
 }
 export default GridTest;
